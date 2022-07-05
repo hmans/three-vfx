2189ec9289bf7aef2699d9b4628bb7dd38623c9a
Hendrik Mans <hendrik@mans.de>
2022-07-05 11:44:14 +0200

    Fix playground

--- apps/examples/src/examples/Playground.tsx
+++ apps/examples/src/examples/Playground.tsx
@@ -20,59 +20,6 @@ import {
 import { Color, ShaderMaterial } from "three"
 import { useShader } from "./useShader"
 
-const WobbleNode = Factory(() => ({
-  name: "Wobble",
-
-  inputs: {
-    /** The time offset */
-    time: float(),
-
-    /** How fast to wobble */
-    frequency: float(1),
-
-    /** How hard to wobble */
-    amplitude: float(1)
-  },
-
-  outputs: {
-    /** The wobble value */
-    value: float("sin(inputs.time * inputs.frequency) * inputs.amplitude")
-  }
-}))
-
-type WobbleProps = {
-  frequency?: number
-  amplitude?: number
-  time?: Parameter<"float">
-}
-
-const WobbleAnimation = ({
-  frequency = 1,
-  amplitude = 1,
-  time = TimeNode()
-}: WobbleProps) =>
-  ComposeNode({
-    x: WobbleNode({ time, frequency: 8 * frequency, amplitude }),
-    y: WobbleNode({ time, frequency: 5 * frequency, amplitude }),
-    z: WobbleNode({ time, frequency: 3 * frequency, amplitude })
-  }).outputs.vector3
-
-const Squeezed = Factory(() => ({
-  inputs: {
-    position: vec3(),
-    time: float(),
-    uv: vec2(UVNode())
-  },
-  outputs: {
-    value: vec3(
-      `vec3(
-        inputs.position.x * (1.0 + sin(inputs.time * 2.0 + inputs.uv.y * 13.0) * 0.3),
-        inputs.position.y * (1.0 + cos(inputs.time * 2.3 + inputs.uv.y * 11.0) * 0.2),
-        inputs.position.z * (1.0 + sin(inputs.time * 2.0 + inputs.uv.y * 13.0) * 0.1))`
-    )
-  }
-}))
-
 export default function Playground() {
   const shaderProps = useShader(() => {
     const colorA = ColorNode({ a: new Color("#f33") })
@@ -90,7 +37,7 @@ export default function Playground() {
     })
 
     const l = AddNode({
-      a: float(), // eeeh
+      a: vec3(), // eeeh
       b: colorB
     })
 
