acc0d93d33157492517d8efefc26844cdc8a810a
Hendrik Mans <hendrik@mans.de>
2022-07-05 11:41:27 +0200

    play with that playground

--- apps/examples/src/examples/Playground.tsx
+++ apps/examples/src/examples/Playground.tsx
@@ -1,5 +1,13 @@
 import { useMemo } from "react"
-import { MeshStandardMaterial, Vector4 } from "three"
+import {
+  Color,
+  Matrix3,
+  Matrix4,
+  MeshStandardMaterial,
+  Vector2,
+  Vector3,
+  Vector4
+} from "three"
 import CustomShaderMaterial from "three-custom-shader-material"
 
 /*
@@ -27,17 +35,55 @@ function statement(...parts: Parts) {
   return [...parts, ";"]
 }
 
-function assignment(name: string, value: any) {
-  return statement(`${name} = ${renderValue(value)}`)
+function assignment(name: string, value: string) {
+  return statement(`${name} = ${value}`)
 }
 
-function renderValue(value: any) {
+function renderValue(value: any): string {
   if (typeof value === "string") {
     return value
+  } else if (isVariable(value)) {
+    return value.name
   } else if (value instanceof Vector4) {
     return `vec4(${value.x}, ${value.y}, ${value.z}, ${value.w})`
   } else {
-    return value
+    throw new Error("Could not render value:", value)
+  }
+}
+
+type ValueType = "bool" | "float" | "vec2" | "vec3" | "vec4" | "mat3" | "mat4"
+
+type JSTypeMap = {
+  bool: boolean
+  float: number
+  vec2: Vector2
+  vec3: Vector3 | Color
+  vec4: Vector4
+  mat3: Matrix3
+  mat4: Matrix4
+}
+
+type Variable<T extends ValueType> = {
+  _isVariable: true
+  name: string
+  type: T
+  value?: Value<T>
+}
+
+function isVariable(v: any): v is Variable<ValueType> {
+  return v && v._isVariable
+}
+
+type Value<T extends ValueType> = JSTypeMap[T] | Variable<T>
+
+type Parameter<T extends ValueType> = Value<T>
+
+function variable<T extends ValueType>(type: T, value?: Value<T>): Variable<T> {
+  return {
+    _isVariable: true,
+    name: `var_${Math.floor(Math.random() * 1000000)}`,
+    type,
+    value
   }
 }
 
@@ -46,23 +92,55 @@ type Program = {
   body?: Chunk
 }
 
-type ShaderNode = {
+type ShaderNode<T extends ValueType> = {
+  [key: string]: any
+  value: Variable<T>
+  render: () => RenderedShaderNode
+}
+
+type RenderedShaderNode = {
   name: string
   vertex?: Program
   fragment?: Program
 }
 
-const CSMMasterNode = (): ShaderNode => ({
-  name: "CSM Root",
-  fragment: {
-    body: assignment("csm_DiffuseColor", new Vector4(0.8, 0.5, 0.3, 1.0))
-  }
+const FloatNode = (value: number): ShaderNode<"float"> => ({
+  value: variable("float", value),
+  render: () => ({
+    name: "Float"
+  })
 })
 
-const compileShader = (root: ShaderNode) => {
+const ColorNode = (color: Color): ShaderNode<"vec3"> => ({
+  value: variable("vec3", color),
+  render: () => ({
+    name: "Color"
+  })
+})
+
+type CSMMasterprops = {
+  color: Parameter<"vec3">
+}
+
+const CSMMasterNode = ({ color }: CSMMasterprops): ShaderNode<any> => ({
+  value: variable("bool"), // eh
+  render: () => ({
+    name: "CSM Root",
+    fragment: {
+      body: assignment("csm_DiffuseColor", renderValue(color))
+    }
+  })
+})
+
+const compileShader = (root: ShaderNode<any>) => {
+  const rendered = root.render()
+
   const vertexShader = ``
 
-  const fragmentShader = concatenate("void main()", block(root.fragment?.body))
+  const fragmentShader = concatenate(
+    "void main()",
+    block(rendered.fragment?.body)
+  )
 
   return {
     vertexShader,
@@ -72,7 +150,9 @@ const compileShader = (root: ShaderNode) => {
 
 export default function Playground() {
   const shader = useMemo(() => {
-    const root = CSMMasterNode()
+    const root = CSMMasterNode({
+      color: ColorNode(new Color("hotpink")).value
+    })
     const shader = compileShader(root)
     return shader
   }, [])
